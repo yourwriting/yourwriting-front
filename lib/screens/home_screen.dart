92d21afaddc5d332ae59f1d23b2f154b497b46c8
leejin_rho <52371699+leejin-rho@users.noreply.github.com>
2023-05-21 03:31:13 +0900

    input label

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,11 +6,24 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: Container(
+        color: Color.fromARGB(255, 147, 171, 207),
+        child: Center(
+          child: Padding(
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'writing',
+                hintText: '오늘의 글을 적어보세요.',
+              ),
+            ),
+            padding: EdgeInsets.all(20.0),
+          ),
+        ),
+      ),
       appBar: AppBar(
         elevation: 2,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
+        backgroundColor: Color.fromARGB(255, 63, 54, 141),
+        foregroundColor: Color.fromARGB(255, 255, 255, 255),
         title: const Text(
           "realWriting",
           style: TextStyle(
