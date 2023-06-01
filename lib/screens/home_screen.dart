9e30b6be2a3f775f08b16ac602e727f01c82f679
leejin_rho <52371699+leejin-rho@users.noreply.github.com>
2023-06-02 05:30:22 +0900

    main화면 윗부분

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,30 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:realwriting/style.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: ColorStyles.mainbackground,
         body: Padding(
-          padding: EdgeInsets.symmetric(
+          padding: const EdgeInsets.symmetric(
             horizontal: 26,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              SizedBox(
-                height: 80,
+              const SizedBox(
+                height: 70,
               ),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Text(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: ColorStyles.mainwhite,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                      child: Text(
+                        'my font',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const Icon(
+                    Icons.remove_circle_outline,
+                    size: 30,
+                    color: ColorStyles.mainwhite,
+                  ),
+                  const SizedBox(
+                    width: 64,
+                  ),
+                  const Text(
                     '2023-05-31',
                     style: TextStyle(
                       color: ColorStyles.mainwhite,
                       fontSize: 20,
                     ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const Icon(
+                    Icons.settings,
+                    size: 55,
+                    color: ColorStyles.mainwhite,
                   ),
                 ],
               ),
