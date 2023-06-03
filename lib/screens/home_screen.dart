44d408caf8f64cc5f48421315b983a2dbe6e7b1d
leejin_rho <52371699+leejin-rho@users.noreply.github.com>
2023-06-04 02:36:14 +0900

    landing page timer nevigation + buttons nevigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:realwriting/screens/generator_screen.dart';
+import 'package:realwriting/screens/wrtingpage_screen.dart';
 import 'package:realwriting/style.dart';
 import 'package:realwriting/widget/book.dart';
 import 'package:dotted_border/dotted_border.dart';
@@ -25,37 +27,44 @@ class HomeScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(13),
-                        boxShadow: [
-                          BoxShadow(
-                              color: ColorStyles.mainshadow.withOpacity(0.5),
-                              blurRadius: 4,
-                              offset: const Offset(0, 2))
-                        ],
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute<void>(
+                            builder: (BuildContext context) {
+                          return const GeneratorScreen();
+                        }));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: ColorStyles.mainblack,
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                        ),
+                        padding: const EdgeInsets.symmetric(
                           vertical: 11.5,
                           horizontal: 23.5,
                         ),
-                        child: Text(
-                          'my font',
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
+                      ),
+                      child: const Text(
+                        'my font',
+                        textAlign: TextAlign.center,
                       ),
                     ),
                     const SizedBox(
                       width: 8,
                     ),
-                    Icon(
-                      Icons.remove_circle_outline,
-                      size: 33,
-                      color: ColorStyles.mainblack.withOpacity(0.8),
+                    Transform.translate(
+                      offset: const Offset(0, 5),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.remove_circle_outline,
+                            size: 33,
+                            color: ColorStyles.mainblack.withOpacity(0.8),
+                          )),
                     ),
                     const SizedBox(
                       width: 50,
@@ -131,12 +140,21 @@ class HomeScreen extends StatelessWidget {
                               borderType: BorderType.RRect,
                               radius: const Radius.circular(20),
                               strokeCap: StrokeCap.round,
-                              child: const Padding(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: 70,
-                                  horizontal: 54,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 63,
+                                  horizontal: 47,
                                 ),
-                                child: Icon(Icons.add),
+                                child: IconButton(
+                                  onPressed: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute<void>(
+                                            builder: (BuildContext context) {
+                                      return const WritingScreen();
+                                    }));
+                                  },
+                                  icon: const Icon(Icons.add),
+                                ),
                               ),
                             ),
                           ),
