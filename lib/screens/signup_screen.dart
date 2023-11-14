49006382ff348e2c7f6a67a688367955ca957e8e
leejin_rho <tbdpapdl@gmail.com>
2023-11-14 18:31:55 +0900

    fix: signup_screen 뒤로가기 버튼 추가

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -66,160 +66,196 @@ class _SignUpScreenState extends State<SignUpScreen> {
       home: Scaffold(
         backgroundColor: ColorStyles.mainbackground,
         body: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 37,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Image(
-                    image: AssetImage('assets/images/realwriting.png'),
-                    width: 178,
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  const Row(
-                    children: [
-                      Text(
-                        'ID',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: ColorStyles.mainblack,
-                            fontSize: 18,
-                            fontFamily: "SF-Pro-Rounded-Semibold"),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  TextFormField(
-                    controller: _loginIdController,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your ID',
-                      hintStyle: TextStyle(
-                        fontSize: 15,
-                        fontFamily: "SF-Pro-Rounded-Regular",
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your ID';
-                      }
-                      return null;
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 55,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
                     },
-                  ),
-                  const SizedBox(
-                    height: 36,
-                  ),
-                  const Row(
-                    children: [
-                      Text(
-                        'Password',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: ColorStyles.mainblack,
-                            fontSize: 18,
-                            fontFamily: "SF-Pro-Rounded-Semibold"),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your password',
-                      hintStyle: TextStyle(
-                        fontSize: 15,
-                        fontFamily: "SF-Pro-Rounded-Regular",
-                      ),
+                    child: const Row(
+                      children: [
+                        SizedBox(
+                          width: 9,
+                        ),
+                        Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          color: ColorStyles.blue,
+                          size: 30,
+                        ),
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                              fontFamily: 'SF-Pro-Display-Regular',
+                              fontSize: 17,
+                              color: ColorStyles.blue),
+                        )
+                      ],
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
                   ),
-                  const SizedBox(
-                    height: 36,
-                  ),
-                  const Row(
-                    children: [
-                      Text(
-                        'Nickname',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            color: ColorStyles.mainblack,
-                            fontSize: 18,
-                            fontFamily: "SF-Pro-Rounded-Semibold"),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  TextFormField(
-                    controller: _nicknameController,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your nickname',
-                      hintStyle: TextStyle(
-                        fontSize: 15,
-                        fontFamily: "SF-Pro-Rounded-Regular",
-                      ),
+                ),
+                const SizedBox(
+                  height: 65,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 37,
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your nickname';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  InkWell(
-                      onTap: () async {
-                        if (_formKey.currentState!.validate()) {
-                          try {
-                            await signup(
-                              _loginIdController.text,
-                              _passwordController.text,
-                              _nicknameController.text,
-                            );
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Image(
+                          image: AssetImage('assets/images/realwriting.png'),
+                          width: 178,
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        const Row(
+                          children: [
+                            Text(
+                              'ID',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: ColorStyles.mainblack,
+                                  fontSize: 18,
+                                  fontFamily: "SF-Pro-Rounded-Semibold"),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        TextFormField(
+                          controller: _loginIdController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your ID',
+                            hintStyle: TextStyle(
+                              fontSize: 15,
+                              fontFamily: "SF-Pro-Rounded-Regular",
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your ID';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 36,
+                        ),
+                        const Row(
+                          children: [
+                            Text(
+                              'Password',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: ColorStyles.mainblack,
+                                  fontSize: 18,
+                                  fontFamily: "SF-Pro-Rounded-Semibold"),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your password',
+                            hintStyle: TextStyle(
+                              fontSize: 15,
+                              fontFamily: "SF-Pro-Rounded-Regular",
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 36,
+                        ),
+                        const Row(
+                          children: [
+                            Text(
+                              'Nickname',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: ColorStyles.mainblack,
+                                  fontSize: 18,
+                                  fontFamily: "SF-Pro-Rounded-Semibold"),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        TextFormField(
+                          controller: _nicknameController,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your nickname',
+                            hintStyle: TextStyle(
+                              fontSize: 15,
+                              fontFamily: "SF-Pro-Rounded-Regular",
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your nickname';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        InkWell(
+                            onTap: () async {
+                              if (_formKey.currentState!.validate()) {
+                                try {
+                                  await signup(
+                                    _loginIdController.text,
+                                    _passwordController.text,
+                                    _nicknameController.text,
+                                  );
 
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const LoginScreen(),
-                              ),
-                            );
-                          } catch (e) {
-                            print('Failed to signup: $e');
-                          }
-                        }
-                      },
-                      child: const Image(
-                        image: AssetImage('assets/images/signup.png'),
-                        width: 327,
-                        height: 50,
-                      )),
-                  const SizedBox(
-                    height: 20,
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const LoginScreen(),
+                                    ),
+                                  );
+                                } catch (e) {
+                                  print('Failed to signup: $e');
+                                }
+                              }
+                            },
+                            child: const Image(
+                              image: AssetImage('assets/images/signup.png'),
+                              width: 327,
+                              height: 50,
+                            )),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ),
+                ),
+              ],
+            )),
       ),
     );
   }
